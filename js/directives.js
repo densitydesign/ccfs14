9c7d3f91b5184d057fcc32c90f4101202f98da20
gianni <scnepa222@gmail.com>
2014-03-24 16:43:59 +0100

    added static map

--- js/directives.js
+++ js/directives.js
@@ -3,9 +3,19 @@
 /* Directives */
 
 
-angular.module('ccfs14.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('ccfs14.directives', [])
+  .directive('mapContainer',[ 'fileService', '$timeout', function (fileService, $timeout){
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl: 'partials/mapcontainer.html',
+      link: function(scope, element, attrs) {
+        console.log(element.width(), element.height())
+        var map = ccfs.map()
+                    .width(element.width())
+                    .height(element.height())
+
+        var chart = d3.select(element[0]).call(map)
+      }
+    }
+  }])
