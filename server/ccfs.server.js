c6066460cc4034779ed8850054c80087817c85f8
Daniele Ciminieri <stonecoldcrazy@gmail.com>
2014-04-03 11:43:56 +0200

    added data

--- server/ccfs.server.js
+++ server/ccfs.server.js
@@ -1,21 +1,42 @@
 var express = require('express');
 var app = express();
 var server = app.listen(8000);
-
+var fs=require('fs');
 var io = require('socket.io').listen(server);
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
-});
+});*/
 
-io.set("origins","*:*")
+//io.set("origins","*:*")
+
+var data_url="../sample_data/"
+var map_url=data_url+"map/"
+
+//map utils - builds folder structure
+var districts={"general":[]}
+var wholemap=fs.readdirSync(map_url)
+for(i in wholemap) {
+	if(fs.lstatSync(map_url+wholemap[i]).isDirectory()) {
+		var distName=wholemap[i]
+		districts[distName]=[]
+		var currDistr =	fs.readdirSync(map_url+distName)
+		for(j in currDistr){
+			districts[distName].push(currDistr[j])
+		}
+	}
+	else {
+		districts.general.push(wholemap[i])
+	}
+}
+
+console.log(districts)
+
 io.sockets.on('connection', function (socket) {
-	console.log("lol")
-	incr = 0;
+	
 	var sendData = function() {
 		data = new Date();
 		socket.emit('send', data);
-		incr++;
 	}
 	var run = setInterval(sendData, 2000);
   socket.on('disconnect', function() {
